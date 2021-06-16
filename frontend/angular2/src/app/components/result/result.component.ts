6cd381bc87bf4f6bec1b5db85f948d6c1b3a06b7
Cristina Prieto Jalao <cristinaprja@gmail.com>
2021-06-16 11:39:06 +0200

    carpetas que faltan de angular

---
+++ frontend/angular2/src/app/components/result/result.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { DataService } from 'src/app/services/data.service';
+import { HomeComponent } from '../home/home.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styleUrls: ['../home/home.component.html','./result.component.scss']
+})
+export class ResultComponent extends HomeComponent implements OnInit {
+  score:number;
+  checkup:boolean;
+  constructor(
+    public authService:AuthService,
+    public userService:UserService,
+    public dataService:DataService,
+    private router:Router,
+    private route:ActivatedRoute,
+  ) {
+    super(authService, userService);
+  }
+
+  ngOnInit(): void {
+    this.getDatos();
+  }
+  getDatos(){
+    this.route.params.subscribe(params => {
+      if(params['score']){
+        this.score = params.score;
+      }
+      if(params['checkup'] == true){
+        this.checkup = true;
+      }else{
+        this.checkup = false;
+      }
+    });
+  }
+
+}
